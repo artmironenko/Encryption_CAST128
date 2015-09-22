a15bb750bd3020fb439c44241e210d919a90f067
fenderil <fenderil@yandex.ru>
2015-09-22 13:07:06 +0300

    Added interface

--- app/src/main/java/ru/fenderil/encryption_cast128/MainActivity.java
+++ app/src/main/java/ru/fenderil/encryption_cast128/MainActivity.java
@@ -4,13 +4,54 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button encoding;
+    Button decoding;
+    int[] cyphertext;
+    EditText originText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        encoding = (Button) findViewById(R.id.encoding);
+        decoding = (Button) findViewById(R.id.decoding);
+        encoding.setOnClickListener(this);
+        decoding.setOnClickListener(this);
+        originText = (EditText) findViewById(R.id.originText);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.encoding:
+                try {
+                    cyphertext = CAST128.encoding(getResources().getString(R.string.originText), getResources().getString(R.string.player1), getResources().getString(R.string.player2));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.decoding:
+                try {
+                    String answer = CAST128.decoding(cyphertext,getResources().getString(R.string.player1),getResources().getString(R.string.player2));
+                    originText.setText(answer);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
